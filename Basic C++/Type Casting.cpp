02a7526cd9c638f7a91d9492893b6d33efb06081
Mohit-Raje <110410869+Mohit-Raje@users.noreply.github.com>
2023-04-23 10:28:10 +0530

    Add files via upload

---
+++ Basic C++/Type Casting.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 'a';
+    cout<< a <<endl;
+
+    char ch = 98;
+    cout<< ch << endl;
+
+    int b = 'b';
+    cout<< b <<endl;
+
+    unsigned int c=-20;
+    cout<< c <<endl;
+
+
+    return 0;
+
+}

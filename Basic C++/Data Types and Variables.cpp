f35b5e05d9a1428ae6bde60bbba6acc55cede32c
Mohit-Raje <110410869+Mohit-Raje@users.noreply.github.com>
2023-04-23 10:10:21 +0530

    Add files via upload

---
+++ Basic C++/Data Types and Variables.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=123;
+
+    cout<<a<<endl;
+
+    char ch='a';
+    cout<<ch<<endl;
+
+    bool bl=true;
+    cout<<bl<<endl;
+
+    float f=3.142;
+    cout<<f<<endl;
+
+    double d=1.412;
+    cout<<d<<endl;
+
+    int size=sizeof(a);
+    cout<<"Size of a is "<<size <<endl;
+
+    int size1=sizeof(ch);
+    cout<<"Size of character is "<<size1<<endl;
+
+    return 0;
+
+
+}
